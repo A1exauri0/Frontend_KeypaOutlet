0b8ca796ed9fddfdf1b9c7e1695091754e2b842d
Adrián Vázquez <adrianalejandrovazquez@gmail.com>
2025-05-28 09:59:47 -0600

    crud v1.1 y flowbite-icons

--- src/components/Admin/crud.vue
+++ src/components/Admin/crud.vue
@@ -1,20 +1,81 @@
 <template>
     <div class="pt-20 sm:pt-16 p-6 sm:ml-64 bg-white dark:bg-purple-900 min-h-screen">
         <!-- Barra de herramientas -->
-        <div class="flex flex-wrap justify-between items-center pb-4">
-            <!-- Aquí puedes meter filtros o dropdown como slot -->
+        <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
+            <!-- Filtro de fecha -->
+            <div>
+                <button id="dropdownRadioButton" data-dropdown-toggle="dropdownRadio"
+                    class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                    type="button">
+                    <IconClockOutline class="w-4 h-4 text-gray-500 dark:text-gray-400 me-3" />
+                    Últimos 30 días
+                    <IconAngleDownOutline class="w-4 h-4 text-gray-500 dark:text-gray-400 ms-2" />
+                </button>
+                <!-- Dropdown menu -->
+                <div id="dropdownRadio"
+                    class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600"
+                    data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="top"
+                    style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);">
+                    <ul class="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
+                        aria-labelledby="dropdownRadioButton">
+                        <li>
+                            <div class="flex items-center p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
+                                <input id="filter-radio-example-1" type="radio" value="" name="filter-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="filter-radio-example-1"
+                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded-sm dark:text-gray-300">Último
+                                    día</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
+                                <input :checked="true" id="filter-radio-example-2" type="radio" value=""
+                                    name="filter-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="filter-radio-example-2"
+                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded-sm dark:text-gray-300">Últimos
+                                    7
+                                    días</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
+                                <input id="filter-radio-example-3" type="radio" value="" name="filter-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="filter-radio-example-3"
+                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded-sm dark:text-gray-300">Últimos
+                                    30
+                                    días</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
+                                <input id="filter-radio-example-4" type="radio" value="" name="filter-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="filter-radio-example-4"
+                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded-sm dark:text-gray-300">Último
+                                    mes</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex items-center p-2 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
+                                <input id="filter-radio-example-5" type="radio" value="" name="filter-radio"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="filter-radio-example-5"
+                                    class="w-full ms-2 text-sm font-medium text-gray-900 rounded-sm dark:text-gray-300">Último
+                                    año</label>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <slot name="toolbar"></slot>
 
             <!-- Buscador -->
             <div class="relative">
                 <div
                     class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
-                    <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                            clip-rule="evenodd"></path>
-                    </svg>
+                    <IconSearchOutline class="w-5 h-5 text-gray-500 dark:text-gray-400" />
                 </div>
                 <input type="text" v-model="search" id="table-search"
                     class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -26,6 +87,7 @@
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
+                    <!-- Checkbox fila titulo-->
                     <th scope="col" class="p-4">
                         <div class="flex items-center">
                             <input id="checkbox-all-search" type="checkbox"
@@ -33,43 +95,41 @@
                             <label for="checkbox-all-search" class="sr-only">checkbox</label>
                         </div>
                     </th>
-                    <th v-for="col in headers" :key="col.key" class="px-6 py-3">{{ col.label }}</th>
-                    <th scope="col" class="px-6 py-3">Acciones</th>
+                    <slot name="columns-header" />
+                    <th class="px-6 py-3">Acciones</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(item, index) in filteredItems" :key="index"
                     class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <!-- Checkbox filas-->
                     <td class="w-4 p-4">
                         <div class="flex items-center">
-                            <input :id="'checkbox-table-search-' + index" type="checkbox"
-                                class="w-4 h-4 text-blue-600 ..." />
-                            <label :for="'checkbox-table-search-' + index" class="sr-only">checkbox</label>
-
+                            <input :id="'checkbox-table-search-' + n" type="checkbox"
+                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            <label :for="'checkbox-table-search-' + n" class="sr-only">checkbox</label>
                         </div>
                     </td>
-                    <td v-for="col in headers" :key="col.key" class="px-6 py-4">{{ item[col.key] }}</td>
+                    <slot name="columns" :item="item" />
                     <td class="px-6 py-4">
                         <slot name="actions" :item="item" />
                     </td>
                 </tr>
             </tbody>
         </table>
+
+        <!-- Paginación -->
         <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
             aria-label="Table navigation">
             <span
-                class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Mostrando
-                <span class="font-semibold text-gray-900 dark:text-white">1-10</span> de <span
+                class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing
+                <span class="font-semibold text-gray-900 dark:text-white">1-10</span> of <span
                     class="font-semibold text-gray-900 dark:text-white">1000</span></span>
             <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
                 <li>
                     <a href="#"
                         class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                        <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m14 8-4 4 4 4" />
-                        </svg>
+                        <IconAngleLeftOutline class="w-4 h-4 mx-auto text-gray-500 dark:text-gray-400" />
                     </a>
                 </li>
                 <li>
@@ -94,7 +154,9 @@
                 </li>
                 <li>
                     <a href="#"
-                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <IconAngleRightOutline class="w-4 h-4 mx-auto text-gray-500 dark:text-gray-400" />
+                    </a>
                 </li>
             </ul>
         </nav>
@@ -102,19 +164,28 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { ref, computed } from 'vue';
+import {
+    IconSearchOutline,
+    IconClockOutline,
+    IconAngleDownOutline,
+    IconAngleLeftOutline,
+    IconAngleRightOutline
+} from '@iconify-prerendered/vue-flowbite'
 
 const props = defineProps({
-    headers: Array, // [{ key: 'nombre', label: 'Nombre' }]
-    items: Array // [{ nombre: '', descripcion: '', ... }]
+    items: {
+        type: Array,
+        required: true
+    }
 });
 
 const search = ref('');
 
 const filteredItems = computed(() => {
     return props.items.filter(item =>
-        Object.values(item).some(value =>
-            String(value).toLowerCase().includes(search.value.toLowerCase())
+        Object.values(item).some(val =>
+            String(val).toLowerCase().includes(search.value.toLowerCase())
         )
     );
 });
